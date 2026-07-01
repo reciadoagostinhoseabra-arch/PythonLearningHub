d48e6291e93a3b320c37f8de94877604b4a19283
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2026-07-01 22:07:04 +0100

    first commit

---
+++ Aula 1/olaMundo.py
@@ -0,0 +1,14 @@
+idade = 6
+nome = "Lukoki Daniel"
+isValid = False
+lista = [1,3,"Lukoki","reciado"]
+dicionario = {
+    "nome": "Lukoki",
+    "idade": 20,
+    "profissao": "Mecanico",
+    "nacionalidade": "Angolano"
+}
+
+print(lista[0])
+print(dicionario)
+print("Ola, Mundo chamo-me",nome,"eu tenho",idade, "Anos de idade")
