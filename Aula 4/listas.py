cd0da3948af0de37fb15dc304acf8be3a4ee3942
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2026-07-05 21:52:07 +0100

    aula 4

---
+++ Aula 4/listas.py
@@ -0,0 +1,8 @@
+myList1 = []
+mylist2 =[1,2,3,"Ana"]
+#myList1.append("Reciado")
+#print(myList1)
+#mylist2.remove(2)
+#del mylist2[2]
+
+print(mylist2)
---
+++ Aula 4/strings.py
@@ -0,0 +1,8 @@
+nome = "Lukoki Daniel"
+##print("colocando em minusculo ",nome.lower())
+##print("colocando em maiuscula ",nome.upper())
+##nomes = "lukoki,reciado,tamara,julia,andrade,filipe"
+##nomesVector = nomes.split(",")
+##print(nomesVector)
+
+print(nome.center(50))
