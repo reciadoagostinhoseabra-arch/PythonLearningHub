1ba65af3f608f2808474dcc33e4a6f1543e8ce02
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2026-07-15 20:55:39 +0100

    aula 7

---
+++ Aula 7/try.py
@@ -0,0 +1,11 @@
+n1 = int(input("digite o primeiro numero "))
+n2 = int(input("digite o segundo  numero "))
+
+try:
+    result = n1/n2
+    print("o resultado é ",result)
+except:
+    print("nao se pode dividir com o numero zero")
+
+
+n3 = int(input("digite o terceiro  numero "))
